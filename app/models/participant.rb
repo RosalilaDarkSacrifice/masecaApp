5dee4eb5a3b47e8d3b28daf4222a3bd7b6ccb0fa
Jerzon Hernandez <joel.hz24@gmail.com>
2012-09-18 13:23:21 -0600

    minucioso#8 {desvalidacion de codigos xD y al fin el banner con

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,6 +1,7 @@
 class Participant < ActiveRecord::Base
   attr_accessible :codigo, :ciudad, :identidad, :nombre, :producto, :telefono
 	validates_presence_of :codigo, :ciudad, :identidad, :nombre, :producto, :telefono
-	validates_uniqueness_of :codigo
+	# jajaja # validates_uniqueness_of :codigo
 	validates_length_of :identidad, :minimum => 13, :maximum => 13
+  validates_length_of :codigo, :minimum => 7, :maximum => 7
 end
